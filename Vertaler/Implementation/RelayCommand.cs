caa96fd80685de61d7fa085a585b4371a2ae7298
Marc Rousavy <marcrousavy@hotmail.com>
2018-05-08 13:18:38 +0200

    Add non-generic RelayCommand

--- Vertaler/Implementation/RelayCommand.cs
+++ Vertaler/Implementation/RelayCommand.cs
@@ -5,7 +5,7 @@ namespace Vertaler.Implementation
 {
     /// <inheritdoc />
     /// <summary>
-    ///     A <see cref="T:System.Windows.Input.ICommand" /> implementation
+    ///     A type safe <see cref="T:System.Windows.Input.ICommand" /> implementation
     /// </summary>
     /// <typeparam name="T">The type of the parameter</typeparam>
     public class RelayCommand<T> : ICommand
@@ -16,14 +16,25 @@ namespace Vertaler.Implementation
         public event EventHandler CanExecuteChanged;
 
         /// <summary>
+        ///     Initialize a new instance of the <see cref="RelayCommand{T}"/>, a <see cref="ICommand"/> implementation,
+        ///     where <see cref="CanExecute"/> is always true
+        /// </summary>
+        /// <param name="execute">The callback to execute on command execution</param>
+        public RelayCommand(Action<T> execute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = arg => true;
+        }
+
+        /// <summary>
         ///     Initialize a new instance of the <see cref="RelayCommand{T}"/>, a <see cref="ICommand"/> implementation
         /// </summary>
         /// <param name="execute">The callback to execute on command execution</param>
         /// <param name="canExecute">The callback to check if this command can execute</param>
         public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
         {
-            _execute = execute;
-            _canExecute = canExecute;
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
         }
 
         /// <inheritdoc />
@@ -53,9 +64,62 @@ namespace Vertaler.Implementation
         /// <summary>
         ///     Raise the <see cref="CanExecuteChanged"/> event
         /// </summary>
-        public void RaiseCanExecuteChanged()
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, new EventArgs());
+    }
+
+
+
+    /// <inheritdoc />
+    /// <summary>
+    ///     A <see cref="T:System.Windows.Input.ICommand" /> implementation
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        ///     Initialize a new instance of the <see cref="RelayCommand"/>, a <see cref="ICommand"/> implementation,
+        ///     where <see cref="CanExecute"/> is always true
+        /// </summary>
+        /// <param name="execute">The callback to execute on command execution</param>
+        public RelayCommand(Action<object> execute)
         {
-            CanExecuteChanged?.Invoke(this, new EventArgs());
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = arg => true;
         }
+
+        /// <summary>
+        ///     Initialize a new instance of the <see cref="RelayCommand"/>, a <see cref="ICommand"/> implementation
+        /// </summary>
+        /// <param name="execute">The callback to execute on command execution</param>
+        /// <param name="canExecute">The callback to check if this command can execute</param>
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     Check if this command can execute with the given parameter
+        /// </summary>
+        /// <param name="parameter">The parameter object</param>
+        /// <returns>If this command can execute with this parameter</returns>
+        public bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? false;
+
+        /// <inheritdoc />
+        /// <summary>
+        ///     Execute the command with the given parameter
+        /// </summary>
+        /// <param name="parameter">The parameter object</param>
+        public void Execute(object parameter) => _execute?.Invoke(parameter);
+
+        /// <summary>
+        ///     Raise the <see cref="CanExecuteChanged"/> event
+        /// </summary>
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, new EventArgs());
     }
 }
