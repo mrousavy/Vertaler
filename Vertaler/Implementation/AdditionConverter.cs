c6fce574af482c07325b7186ccc2d8445ef53f55
Marc Rousavy <marcrousavy@hotmail.com>
2018-05-08 15:08:49 +0200

    Add Addition Converter

---
+++ Vertaler/Implementation/AdditionConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Vertaler.Implementation
+{
+    [ValueConversion(typeof(double), typeof(double), ParameterType = typeof(double))]
+    public class AdditionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool parsed = double.TryParse(parameter?.ToString(), out double operand);
+
+            switch (value)
+            {
+                case double d:
+                    return parsed ? d + operand : d;
+                case int i:
+                    return parsed ? i + operand : i;
+                default:
+                    return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool parsed = double.TryParse(parameter?.ToString(), out double operand);
+
+            switch (value)
+            {
+                case double d:
+                    return parsed ? d + operand : d;
+                case int i:
+                    return parsed ? i + operand : i;
+                default:
+                    return value;
+            }
+        }
+    }
+}
